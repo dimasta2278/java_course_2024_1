3a9c10f1d63a95b026f791df53b5b5f0de8651ff
dimasta2278@gmail.com <dimasta2278@gmail.com>
2025-01-11 23:50:10 +0500

    lr 3

--- src/lr3/example8.java
+++ src/lr3/example8.java
@@ -11,6 +11,18 @@ public class example8 {
         // заполнение массива буквами через одну
         for (int i = 0; i < size; i++)
             charArray[i] = (char) ('A' + i);
-        System.out.println(Arrays.toString(charArray));
+        System.out.println("Исходный массив: " + Arrays.toString(charArray));
+
+        // Удаление гласных
+        char[] filteredArray = removeVowels(charArray);
+        System.out.println("Массив без гласных: " + Arrays.toString(filteredArray));
     }
+
+    //Метод для удаления гласных
+    private static char[] removeVowels(char[] array) {
+        return new String(array)
+                .replaceAll("[AEIOU]", " ")
+                .toCharArray();
+    }
+
 }
