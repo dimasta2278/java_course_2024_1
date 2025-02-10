ebe41c24ce8245a4d8f94d863056958e863acde9
dmitry <dimasta2278@gmail.com>
2025-02-10 21:48:16 +0500

    lr 4

---
+++ src/lr4/example3_1.java
@@ -0,0 +1,20 @@
+package lr4;
+
+public class example3_1 {
+    public static void main(String[] args) {
+        int [][] matrice = {{1,2,3}, {4,5,6}, {7,8,9}};
+        System.out.print(matrice[0][0]);
+        System.out.print(matrice[0][1]);
+        System.out.println(matrice[0][2]);
+        System.out.print(matrice[1][0]);
+        System.out.print(matrice[1][1]);
+        System.out.println(matrice[1][2]);
+        System.out.print(matrice[2][0]);
+        System.out.print(matrice[2][1]);
+        System.out.print(matrice[2][2]);
+    }
+
+
+
+
+}
